b09ae1d1959771edf72504fc8bfc805b2d022979
Muhammad Nuhsyah putra <putramirza712@gmail.com>
2022-07-22 07:39:49 +0800

    update createTable 1 #

--- Createsqltable.js
+++ Createsqltable.js
@@ -1,15 +1,40 @@
+   /*Below are SQL Statements used to create tables
+    Tables  to create:
+    1.User table
+    2.course table 
 
-const user = `
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-    userid SERIAL primary key,
-    userName VARCHAR not null,
-    userPassword VARCHAR not null,
-    userEmail VARCHAR not null,
-    userAddress VARCHAR not null,
-    userContactNumber VARCHAR not null,
-    userRole VARCHAR not null,
-    courseid VARCHAR not null
+
+
+   */
+   
+ 
+const CREATE_SchoolManagementSystem_Table = `
+DROP TABLE IF EXISTS User CASCADE;
+DROP TABLE IF EXISTS Course CASCADE;
+
+
+
+CREATE TABLE IF NOT EXISTS User (
+    Userid SERIAL primary key,
+    UserName VARCHAR not null,
+    UserPassword VARCHAR not null,
+    UserEmail VARCHAR not null,
+    UserAddress VARCHAR not null,
+    UserContactNumber VARCHAR not null,
+    UserRole VARCHAR not null,
+    Courseid INT UNIQUE,
+    CONSTRAINT fk_Course_id FOREIGN KEY(Courseid) REFERENCES Course(Courseid) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS Course (
+    Courseid SERIAL PRIMARY KEY,
+    Coursecode VARCHAR(255) UNIQUE NOT NULL,
+    CourseName VARCHAR(255) NOT NULL,
+    Courseabbrev VARCHAR(255) NOT NULL
+    
+);
+
+
 `;
-module.exports.user = user;
+
+module.exports = CREATE_SchoolManagementSystem_Table;
