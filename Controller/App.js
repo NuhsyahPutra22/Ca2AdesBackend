ac09335979e5b578df23e94f35446588c5f76ebf
Muhammad Nuhsyah putra <putramirza712@gmail.com>
2022-07-22 09:43:14 +0800

    commit Backend Course By Putra #4

---
+++ Controller/App.js
@@ -0,0 +1,12 @@
+
+// Query Functions
+const {
+     GetCourseinfoByID,
+      InsertCourseInfo,
+      DeleteCourseInfoByID,
+      UpdateCourseByID,
+     getAllCourse,
+    } = require("../Model/Course");
+
+    const Course = require("../Model/Course");
+
--- Model/Course.js
+++ Model/Course.js
@@ -1,2 +1,22 @@
 const createHttpError = require('http-errors');
 const { query, POSTGRES_ERROR_CODE } = require("../databaseConn");
+
+const Course ={
+    insertCourseInfo: (Course) => {
+
+        const sql =`INSERT INTO Course( Coursecode,)
+        values($1,$2,$3)
+        RETURNING *`
+    
+        return query(sql, [])
+                .then((result) => {
+                    //console.log(result);
+                    return result;
+                    //function
+                })
+                .catch((error) => {
+                    console.log(error);
+                    throw error;
+                })
+    },
+}
