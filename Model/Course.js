867eab67852d549b8b672b3d7f412fd9bfb7bd32
Muhammad Nuhsyah putra <putramirza712@gmail.com>
2022-07-22 18:41:57 +0800

    Update Course backend #2

--- Model/Course.js
+++ Model/Course.js
@@ -2,7 +2,7 @@ const createHttpError = require('http-errors');
 const { query, POSTGRES_ERROR_CODE } = require("../Database");
 
 module.exports.getAllCourse = function get() {
-    return query(`SELECT * FROM CourseTable  `,[])
+    return query(`SELECT * FROM CourseTable`,[])
     .then((result) => {
         if  (!result.rows.length) return null;
         console.log(result.rows);
